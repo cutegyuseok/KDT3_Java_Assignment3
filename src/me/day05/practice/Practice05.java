cba3c4beddc5706822e16571c51c492e8f06c1d7
cutegyuseok <yigyuseok@gmail.com>
2022-09-26 19:27:34 +0900

    prac 5,6 done

--- src/me/day05/practice/Practice05.java
+++ src/me/day05/practice/Practice05.java
@@ -1,6 +1,26 @@
 package me.day05.practice;
 
+import java.util.Scanner;
+
 public class Practice05 {
     public static void main(String[] args) {
+        int p = 0;
+        int y = 0;
+        Scanner s = new Scanner(System.in);
+        String[] a = s.next().split("");
+        for (int i=0; i < a.length; i++) {
+            String A = a[i].toLowerCase();
+            if (A.equals("p")) {
+                p++;
+            }
+            if (A.equals("y")) {
+                y++;
+            }
+        }
+        if (p == y ){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
     }
 }
--- src/me/day05/practice/Practice06.java
+++ src/me/day05/practice/Practice06.java
@@ -1,4 +1,16 @@
 package me.day05.practice;
 
 public class Practice06 {
+    public static void main(String[] args) {
+
+        String answer = "";
+        String phone_number = "01059101693";
+        int n = phone_number.length();
+        String A = phone_number.substring(n-4);
+        for (int i=0; i<n-4;i++){
+           answer +="*";
+        }
+        answer+=A;
+        System.out.println(answer);
+    }
 }
